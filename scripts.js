e0ef47983a31beceff2d4032d161bae14b4898fc
ArvonVandetook <aaron.tukey@icloud.com>
2025-06-15 15:28:52 -0700

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,49 @@
+
+mapboxgl.accessToken = 'pk.eyJ1IjoibW9ja3VzZXIiLCJhIjoiY2tzc2UyYmIwMDFrdzJ3cDk3ZWh5ZW12dyJ9.q_wb2tNyj5OqEQaBvC-GkA';
+const map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/dark-v10',
+  center: [0, 20],
+  zoom: 1.5
+});
+
+let markers = [];
+let playing = false;
+
+function togglePlayback() {
+  if (playing) {
+    clearMarkers();
+    playing = false;
+  } else {
+    loadEarthquakes();
+    playing = true;
+  }
+}
+
+function loadEarthquakes() {
+  fetch('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson')
+    .then(res => res.json())
+    .then(data => {
+      data.features.forEach((quake, i) => {
+        setTimeout(() => {
+          const coords = quake.geometry.coordinates;
+          const mag = quake.properties.mag;
+          const el = document.createElement('div');
+          el.style.width = `${mag * 4}px`;
+          el.style.height = `${mag * 4}px`;
+          el.style.background = 'rgba(255,100,0,0.7)';
+          el.style.borderRadius = '50%';
+          el.style.boxShadow = '0 0 12px 3px rgba(255,100,0,0.5)';
+          new mapboxgl.Marker(el)
+            .setLngLat([coords[0], coords[1]])
+            .addTo(map);
+          markers.push(el);
+        }, i * 300);
+      });
+    });
+}
+
+function clearMarkers() {
+  document.querySelectorAll('.mapboxgl-marker').forEach(m => m.remove());
+  markers = [];
+}
